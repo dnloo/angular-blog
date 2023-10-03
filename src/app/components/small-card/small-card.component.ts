ad1083e5e9956ec6773f32bbb18c3b483a32a37a
Danilo Guerreiro <danilo482000@yahoo.com.br>
2023-10-03 19:39:10 -0300

    components dinamics propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
-
+  @Input ()
+  photoCover:string = "https://ih1.redbubble.net/image.453958487.1770/pp,840x830-pad,1000x1000,f8f8f8.u3.jpg"
+  @Input ()
+  cardTitle:string = "ccccccccc"
   constructor() { }
 
   ngOnInit(): void {
